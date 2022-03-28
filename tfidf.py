e6fc41ee0c13bd151bc309d83bc7e314d035da75
Matthew Killeen <58102276+MattKilleen@users.noreply.github.com>
2022-03-28 04:40:24 -0400

    Add files via upload

--- tfidf.py
+++ tfidf.py
@@ -48,16 +48,16 @@ def preprocessing():
     return preprocessed_docs
 
 
-def compute_tfidf(docs):
+def compute_tfidf(docs, orig_docs):
     word_occurence_in_docs = Counter()
-    for file in docs:
+    for file_index, file in enumerate(docs):
         with open(file, 'r') as curr_file:
             for line in curr_file:
                 contents = line.split(" ")
                 words = set(contents)
                 word_occurence_in_docs.update(words)
 
-    for file in docs:
+    for file_index, file in enumerate(docs):
         with open(file, 'r') as curr_file:
             for line in curr_file:
                 contents = line.split(" ")
@@ -71,9 +71,29 @@ def compute_tfidf(docs):
                     idf_dict[word] = ((math.log(len(docs) / word_occurence_in_docs[word])) + 1)
                     tf_idf_dict[word] = round((tf_dict[word] * idf_dict[word]), 2)
 
+                tf_idf_list =[(key, value) for key, value in tf_idf_dict.items()]
+                tf_idf_list.sort(key=lambda i: i[0])
+                tf_idf_list.sort(key=lambda i:i[1], reverse=True)
+
+                print(tf_idf_list)
+
+                tfidf_file = 'tfidf_' + orig_docs[file_index]
+                print(tfidf_file)
+                print()
+                with open(tfidf_file, 'w') as write_file:
+                    if len(tfidf_file) <= 5:
+                        write_file.write(str(tf_idf_list))
+                    else:
+                        write_file.write(str(tf_idf_list[:5]))
+
+
 
 if __name__ == '__main__':
+    orig_docs = []
+    for file in open('tfidf_docs.txt', 'r'):
+        file = file.strip('\n')
+        orig_docs.append(file)
     docs = preprocessing()
-    compute_tfidf(docs)
+    compute_tfidf(docs, orig_docs)
 
 
