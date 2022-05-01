e36f23f208a7290e23b6f16eebcea7536227d48f
Matthew Killeen <58102276+MattKilleen@users.noreply.github.com>
2022-05-01 14:23:35 -0400

    Update music_db.sql

--- music_db.sql
+++ music_db.sql
@@ -1,52 +1,83 @@
-drop database musicboi;
-create database if not exists musicboi;
+drop database if exists musicboi;
+create database musicboi;
 use musicboi;
 
 
-create table if not exists Artist(
+create table Artist
+(
+	artist_name varchar(50) primary key
+);
 
-thename varchar(50),
+create table Album
+(
+	album_title varchar(50) primary key,
+	artist_name varchar(50), 
+	released_on_date datetime,
+	constraint album_fk_artist
+		foreign key(artist_name) 
+        references Artist(artist_name)
+);
 
-primary key(thename));
+create table Song
+(
+	song_title varchar(50) primary key, 
+	artist_name varchar(50), 
+	release_date datetime, 
+	album_name varchar(50),
+	constraint song_fk_artist
+		foreign key(artist_name) 
+        references Album(artist_name)
+);
 
-create table if not exists Album(
-thename varchar(50),
-artist_name varchar(50), 
-released_on_date datetime,
-primary key(thename),
-foreign key(artist_name) references Artist(thename));
+create table Genre(
+	genre_name varchar(50) primary key
+);
 
-create table if not exists Song(
-title varchar(50), 
-artist_name varchar(50), 
-release_date datetime, 
-album_name varchar(50),
-primary key(title, artist_name),
-foreign key(artist_name) references Album(artist_name)); -- need to check that reference
+create table Song_Genre
+(
+	song_title varchar (50), 
+	genre_name varchar(50),
+    constraint song_genre_fk_song
+		foreign key(song_title) 
+        references Song(song_title),
+	constraint song_genre_fk_genre
+		foreign key (genre_name)
+        references Genre(genre_name)
+);
 
-create table if not exists Genre(
-thename varchar(50),
-primary key(thename));
+create table MusicUser
+(
+	username varchar(50) primary key
+);
 
-create table if not exists Song_Genre(
-song_title varchar (50), 
-genre_name varchar(50),
-foreign key(song_title, genre_name) references Song(title, artist_name));
+create table Playlist
+(
+	username varchar(50),
+	playlist_title varchar(50) primary key, 
+	song_title varchar(50), 
+	created_date datetime,
+    constraint playlist_fk_musicuser
+		foreign key(username)
+        references MusicUser(username),
+	constraint playlist_fk_song
+		foreign key(song_title) 
+        references Song(song_title)
+);
 
-create table if not exists Song_user(
-username varchar(50),
-primary key (username));
-
-create table if not exists Playlist(
-title varchar(50), 
-song_title varchar(50), 
-created_date datetime,
-primary key(title, created_date),
-foreign key(song_title) references Song(title));
-
-create table if not exists Rating(
-username varchar(50), 
-thename varchar(50), 
-rating int,
-foreign key (thename) references Artist(thename),
-foreign key (username) references Song_user(username));
+create table Rating
+(
+	username varchar(50), 
+	album_title varchar(50),
+    song_title varchar(50),
+    playlist_title varchar(50),
+	rating int,
+    constraint rating_fk_album
+		foreign key (album_title)
+        references Album(album_title),
+	constraint rating_fk_song
+		foreign key (song_title)
+        references Song(song_title),
+	constraint rating_fk_playlist
+		foreign key (playlist_title)
+        references Playlist(playlist_title)
+);
